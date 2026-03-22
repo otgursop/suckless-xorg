2a8f46ea08a907ebc7f9cf3d547b489539abc973
otgursop <otgursop@git.com>
2026-03-22 11:50:17 +0500

    color changed

--- slock/config.h
+++ slock/config.h
@@ -3,10 +3,10 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#050c00",   /* during input */
-	[FAILED] = "#160000",   /* wrong password */
+	[INIT] =   "#282828",     /* after initialization */
+	[INPUT] =  "#504945",   /* during input */
+	[FAILED] = "#1d2021",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
